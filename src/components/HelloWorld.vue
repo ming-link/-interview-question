8f7ca0b83fa23e8cba1835be51c75a865874351b
link <406697908@qq.com>
2020-03-10 00:18:42 +0800

    '1.0'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,317 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="work">
+      <div class="workLeft">
+        <div class="head">
+          <div class="headImg">
+            <img src="../assets/logo.png" alt style="width:100%" />
+          </div>
+          <div class="headCentre">
+            <span class="headFont">Dashboard</span>
+            <span class="headFont">Services</span>
+            <span class="headFont">History</span>
+            <span class="headFont">Actions</span>
+          </div>
+          <div class="hesdRight">
+            <input type="text" class="input" />
+            <span class="button">
+              <img src="../assets/fandajing.png" alt style="width:90%" />
+            </span>
+          </div>
+        </div>
+        <div class="split">
+          <span>Cards</span>
+          <span>View all cards</span>
+        </div>
+        <div style="display: flex;">
+          <div class="card">
+            <p class="cardHead">Available balance</p>
+            <p class="cardPrice">$ 2,200</p>
+            <p class="cardnum">**** **** **** **34</p>
+            <p class="cardname">Monzo</p>
+            <div>
+              <div class="cardun">
+                <div class="carduni carduniLeft"></div>
+                <div class="carduni"></div>
+              </div>
+            </div>
+          </div>
+          <div class="card azury">
+            <p class="cardHead">Available balance</p>
+            <p class="cardPrice">$ 2,200</p>
+            <p class="cardnum">**** **** **** **34</p>
+            <p class="cardname">Monzo</p>
+            <div>
+              <div class="cardun">
+                <div class="carduni carduniLeft"></div>
+                <div class="carduni"></div>
+              </div>
+            </div>
+          </div>
+          <div class="card white">
+            <img src="../assets/jia.png" alt class="addCardImg" />
+            <p class="newCard">Add New Card</p>
+          </div>
+        </div>
+        <div class="flex">
+          <div class="lower">
+            <div class="split">
+              <span class="lowerLeft">Main Services</span>
+              <span class="lowerRight">View all</span>
+            </div>
+            <div class="flex">
+              <div class="lowerFoot">
+                <table></table>
+                <img src="../assets/weibiaoti8.png" alt class="lowerFootImg" />
+                <p class="lowerFootTitle">Transactions</p>
+              </div>
+              <div class="lowerFoot">
+                <table></table>
+                <img src="../assets/huaban.png" alt class="lowerFootImg" />
+                <p class="lowerFootTitle">Transactions</p>
+              </div>
+              <div class="lowerFoot">
+                <table></table>
+                <img src="../assets/tubiaozhizuomoban-.png" alt class="lowerFootImg" />
+                <p class="lowerFootTitle">Transactions</p>
+              </div>
+              <div class="lowerFoot">
+                <table></table>
+                <img src="../assets/weibolu.png" alt class="lowerFootImg" />
+                <p class="lowerFootTitle">Transactions</p>
+              </div>
+              <div class="lowerFoot">
+                <table></table>
+                <img src="../assets/Concise.png" alt class="lowerFootImg" />
+                <p class="lowerFootTitle">Transactions</p>
+              </div>
+              <div class="lowerFoot">
+                <table></table>
+                <img src="../assets/jiantouarrow491.png" alt class="lowerFootImg" />
+                <p class="lowerFootTitle">Transactions</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="split">
+              <span>Current</span>
+              <span>View all</span>
+            </div>
+            <div>
+              <span>
+                1063873637834
+                <span>----------------------------------------------------</span>
+              </span>
+              <span>$2,200.01</span>
+            </div>
+            <div>
+              <span>
+                1063873637834
+                <span>----------------------------------------------------</span>
+              </span>
+              <span>$2,200.01</span>
+            </div>
+            <div>
+              <span>
+                1063873637834
+                <span>----------------------------------------------------</span>
+              </span>
+              <span>$2,200.01</span>
+            </div>
+            <div>
+              <div class="split">
+                <span class="lowerLeft">Main Services</span>
+                <span class="lowerRight">View all</span>
+              </div>
+              <div>
+                <div id="category" :style="{width: '250px', height: '150px'}"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="aside">
+        <Aside />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Aside from "./aside.vue";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  components: {
+    Aside
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    this.drawLine();
+  },
+  methods: {
+    drawLine() {
+      let myChart = this.$echarts.init(document.getElementById("category"));
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello {
+  position: relative;
+  width: 57.5rem;
+  padding-left: 1.875rem;
+  padding-top: 1.25rem;
+  margin: o auto;
+  border: 0.25rem solid rgb(0, 145, 170);
+  border-radius: 2.3rem;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.workLeft {
+  width: 40.875rem;
 }
-li {
+.head {
+  display: flex;
+}
+.headImg {
+  width: 2rem;
+}
+.headFont {
+  font-weight: bold;
+  font-size: 0.875rem;
+}
+.headCentre {
+  margin-left: 3rem;
+  margin-right: 0.6rem;
+  width: 20rem;
+  padding-top: 0.625rem;
+  display: flex;
+  justify-content: space-around;
+}
+.hesdRight {
+  margin-left: 2.5rem;
+  display: flex;
+  justify-content: space-around;
+  background: #eee;
+  border-radius: 1.2rem;
+  height: 30px;
+  overflow: hidden;
+}
+
+.input {
+  border: 0;
+  opacity: 0.5;
+  background: #eee;
+  outline: 0;
+  padding: 0.25rem;
+  padding-left: 0.625rem;
+}
+.button {
+  padding-top: 0.08rem;
+  width: 2.25rem;
+}
+.split {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1.875rem;
+  margin-bottom: 0.9375rem;
+}
+.card {
+  position: relative;
+  width: 11.25rem;
+  height: 6.25rem;
+  background: pink;
+  border-radius: 0.625rem;
+  padding: 0.3125rem;
+  padding-left: 0.9375rem;
+  margin-right: 1.2rem;
+}
+.cardHead {
+  font-size: 0.5rem;
+}
+.cardPrice {
+  margin-top: -0.5rem;
+  color: #fff;
+}
+.cardname {
+  margin-top: -1rem;
+}
+.cardun {
+  margin: 0;
+  width: 2.5rem;
+  height: 1.5rem;
+  position: absolute;
+  border-radius: 0.6rem;
+  top: 4.375rem;
+  right: 0.5rem;
+}
+.carduni {
   display: inline-block;
-  margin: 0 10px;
+  width: 1.25rem;
+  height: 1.5rem;
+  background: #ff0;
+  border-radius: 50%;
 }
-a {
-  color: #42b983;
+.carduniLeft {
+  background: red;
+  margin-right: -0.3125rem;
+}
+.white {
+  background: #eee;
+}
+.azury {
+  background: royalblue;
+}
+.addCardImg {
+  display: block;
+  margin-top: 2rem;
+  margin-left: 4.5rem;
+  width: 2rem;
+}
+.newCard {
+  padding-left: 2.2rem;
+  margin-top: 0.2rem;
+}
+.lower {
+  width: 10.7rem;
+}
+.lowerLeft {
+  font-size: 0.875rem;
+  font-weight: bolder;
+}
+.lowerRight {
+  font-size: 0.625rem;
+  color: #999;
+}
+.flex {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.lowerFoot {
+  width: 5.0625rem;
+  height: 5.0625rem;
+  background: seashell;
+  border-radius: 0.7rem;
+  margin-bottom: 0.5rem;
+}
+
+.lowerFootImg {
+  display: block;
+  margin-top: 0.6rem;
+  margin-left: 1.2rem;
+  width: 2.5rem;
+}
+.lowerFootTitle {
+  padding: 0.4rem;
+  margin-top: 0.15rem;
+  font-size: 0.6rem;
+}
+.aside {
+  position: absolute;
+  top: 0;
+  right: 0;
 }
 </style>
---
+++ src/components/aside.vue
@@ -0,0 +1,196 @@
+<template>
+  <div class="homeRight">
+    <div class="news">
+      <div>
+        <img src="../assets/lingdang.png" alt class="newsImg" />
+        <span></span>
+      </div>
+      <div>
+        <b class="userName">Anastasia</b>
+        <img src="../assets/touxiang.jpeg" alt class="newst" />
+      </div>
+    </div>
+    <div class="pie">
+      <div id="pie" :style="{width: '150px', height: '150px'}"></div>
+      <div class="pieCentre">
+        <b style="font-size:1.2rem">$5,349</b>
+        <br />
+        <span style="font-size:0.6rem;padding-left:0.4rem">Balance</span>
+      </div>
+    </div>
+    <div class="list">
+      <div class="news">
+        <b class="listTopFont">Transactions</b>
+        <b class="listTopFont">
+          Today
+          <img src="../assets/xiajiangtou.png" alt class="listTopImg" />
+        </b>
+      </div>
+      <div>
+        <div class="news top">
+          <img src="../assets/qiu.png" alt class="listImg" />
+          <b class="brief">Dribbble Pro Plan</b>
+          <b class="brief money">- $100</b>
+        </div>
+        <div class="news top">
+          <img src="../assets/san (2).png" alt class="listImg" />
+          <b class="brief">Dribbble Pro Plan</b>
+          <b class="brief money">- $100</b>
+        </div>
+        <div class="news top">
+          <img src="../assets/san (1).png" alt class="listImg" />
+          <b class="brief">Dribbble Pro Plan</b>
+          <b class="brief money">- $100</b>
+        </div>
+      </div>
+      <div class="foot">
+        <button class="button">
+          <span class="buttonIn">Rransfer Money</span>
+          <img src="../assets/jiantou.png" alt class="buttonImg" />
+        </button>
+        <img src alt />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+let echarts = require("echarts/lib/echarts");
+// 引入柱状图组件
+require("echarts/lib/chart/bar");
+// 引入提示框和title组件
+require("echarts/lib/component/tooltip");
+require("echarts/lib/component/title");
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    this.drawLine();
+  },
+  methods: {
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      let pie = this.$echarts.init(document.getElementById("pie"));
+      // 绘制图表
+      pie.setOption({
+        tooltip: {
+          trigger: "item"
+        },
+        legend: {
+          orient: "vertical"
+        },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            radius: ["75%", "100%"],
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                show: false,
+                position: "center"
+              },
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: "30",
+                  fontWeight: "bold"
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            data: [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }]
+          }
+        ]
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.homeRight {
+  width: 13.75rem;
+  height: 33.2rem;
+  border-radius: 2rem;
+  background: white;
+  padding: 0.625rem;
+}
+.news {
+  display: flex;
+  justify-content: space-between;
+}
+.newsImg {
+  width: 1.875rem;
+}
+.newst {
+  width: 2.5rem;
+  border-radius: 50%;
+  vertical-align: middle;
+}
+.userName {
+  display: inline-block;
+  font-size: 0.75rem;
+}
+.pie {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  padding-left: 2.1875rem;
+  position: relative;
+}
+.pieCentre {
+  width: 60px;
+  position: absolute;
+  top: 3.75rem;
+  left: 5.3rem;
+}
+.top {
+  margin-top: 0.9375rem;
+}
+.list {
+  padding: 0.5rem;
+}
+.listTopFont {
+  font-size: 0.75rem;
+  color: #aaa;
+}
+.listTopImg {
+  width: 0.5rem;
+}
+.listImg {
+  width: 1.5rem;
+}
+.brief {
+  display: inline-block;
+  padding-top: 0.2rem;
+  font-size: 0.75rem;
+}
+.foot {
+  margin-top: 1.875rem;
+  position: relative;
+}
+.button {
+  width: 100%;
+  background: #d6204b;
+  padding-left: 1rem;
+  padding-right: 0.8rem;
+  border-radius: 0.9375rem;
+  border: 0;
+  outline: 0;
+  display: flex;
+  justify-content: space-between;
+}
+.buttonIn {
+  display: inline-block;
+  margin-top: 0.3125rem;
+}
+.buttonImg {
+  width: 1.8rem;
+  vertical-align: middle;
+}
+</style>
